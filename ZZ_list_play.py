d151a64607f086e27b58e1268cc7fb2f917b652a
gibsonr50123-eng <gibsonr50123@masseyhigh.school.nz>
2026-09-10 13:30:11 +1200

    Add files via upload

---
+++ ZZ_list_play.py
@@ -0,0 +1,15 @@
+import random
+
+all_numbers = []
+
+for item in range(0, 5):
+
+    # generate a random number
+    mystery_num = random.randint(1, 10)
+    all_numbers.append(mystery_num)
+
+    if mystery_num not in all_numbers:
+        all_numbers.append(mystery_num)
+
+all_numbers.sort()
+print(all_numbers)
